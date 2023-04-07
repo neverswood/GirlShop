d6df85e1ed579a4d29a0bec322c47bedc990c40c
neverswood <nevers_wood@hotmail.fr>
2023-04-07 11:37:09 +0200

    Change the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,10 @@
 import '../styles/Banner.css';
+import bannerLogo1 from '../assets/bannerLogo1.jpg';
 
 function Banner() {
   return (
-    <div className="lmj-banner">
-      {' '}
-      <img src="" alt="Girl shop logo" className="lmj-logo" />
+    <div className="banner">
+      <img src={bannerLogo1} alt="Girl shop logo" className="lmj-logo" />
       <h1 className="lmj-title">Girl shop</h1>
     </div>
   );
