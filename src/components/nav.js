8b51f3089418baa294c3c77f68de8b71235a852a
neverswood <nevers_wood@hotmail.fr>
2023-04-07 11:37:43 +0200

    Change the nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/Nav.css';
 
 export default function Nav() {
   return (
     <>
-      <nav>
+      <nav className="nav">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/dress">Dress</NavLink>
         <NavLink tp="/top">Top</NavLink>
