38be6e9eca2c25d0ec7d43d53868e331916e41bb
neverswood <nevers_wood@hotmail.fr>
2023-05-12 23:05:17 +0200

    Change banner navigation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,20 @@
 import '../styles/Banner.css';
-import bannerLogo1 from '../assets/bannerLogo1.jpg';
+import logo from '../assets/logo.png';
+import user from '../assets/user.png';
+import pan from '../assets/pan.png';
+import menu from '../assets/menu.png';
 
 function Banner() {
   return (
     <div className="banner">
-      <img src={bannerLogo1} alt="Girl shop logo" className="lmj-logo" />
-      <h1 className="lmj-title">Girl shop</h1>
+      <img src={logo} alt="Girl shop logo" className="lmj-logo" />
+      <div className="container-icon">
+        <img src={user} alt="connexion user" className="icon" />
+        <img src={pan} alt="basket" className="icon" />
+      </div>
+      <div className="menu">
+        <img src={menu} alt="menu" className="menu"></img>
+      </div>
     </div>
   );
 }
