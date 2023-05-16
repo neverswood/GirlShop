64df0f1f2b5d3e08c392dfa0c7d5b293f205c881
neverswood <nevers_wood@hotmail.fr>
2023-05-16 14:08:32 +0200

    Add functionalit to subscribe to the newsletter and add contact of the company

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,72 @@
 import { useState } from 'react';
 import '../styles/Footer.css';
+import facebook from '../assets/facebook.png';
+import mail2 from '../assets/mail2.png';
+import insta from '../assets/insta.png';
+import { Link } from 'react-router-dom';
+
+/**
+ * Footer function to subscribe to the newsletter and to contact the company.
+ */
 
 function Footer() {
   const [inputValue, setInputValue] = useState('');
+  const [isShow, setIsShow] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   function handleInput(e) {
     setInputValue(e.target.value);
   }
 
-  function handleBlur() {
+  function handleClick() {
     if (!inputValue.includes('@')) {
-      alert(
-        "Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥"
-      );
+      setIsShow(true);
+    } else {
+      setIsShow(false);
+      setIsOpen(true);
     }
   }
 
   return (
     <footer className="lmj-footer">
-      <div className="lmj-footer-elem">ppppp</div>
-      <div className="lmj-footer-elem">Laissez-nous votre mail :</div>
+      <div className="lmj-footer-elem">Inscivez-vous à la newsletter :</div>
       <input
         placeholder="Entrez votre mail"
         onChange={handleInput}
         value={inputValue}
-        onBlur={handleBlur}
       />
+      <button onClick={handleClick}>Je m'inscris</button>
+      {isShow === true ? (
+        <p className="error">Veuillez renseigner une adresse valide!</p>
+      ) : (
+        ''
+      )}
+      {isOpen === true ? (
+        <div className="modal-newsletter">
+          <div className="close" onClick={() => setIsOpen(false)}>
+            X
+          </div>
+          <h2 className="modal-title">Bienvenue !</h2>
+          <p>
+            Nous vous avons envoyé un e-mail de confirmation pour votre
+            inscription à notre newsletter.
+          </p>
+        </div>
+      ) : (
+        ''
+      )}
+      <div className="contact">
+        <p>Nous contacter : </p>
+        <Link href="mailto:service@girlshop.com">
+          <img src={mail2} alt="Girl shop logo" className="lmj-logo" />
+        </Link>
+        <Link href="" className="facebook">
+          <img src={facebook} alt="Girl shop logo" className="lmj-logo" />
+        </Link>
+        <Link href="">
+          <img src={insta} alt="Girl shop logo" className="lmj-logo" />
+        </Link>
+      </div>
     </footer>
   );
 }
