c0fa9ce40feea6e0e77c5688ae476e99654ebbfc
neverswood <nevers_wood@hotmail.fr>
2023-05-16 14:50:46 +0200

    Change the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,37 @@
 import { Link } from 'react-router-dom';
 import '../styles/Home.css';
+import banner from '../assets/banner6.avif';
+import image from '../assets/banner3.avif';
+import image2 from '../assets/premium_photo-1661548044079-afb3cda91d85.avif';
+import image3 from '../assets/premium_photo-1661578215999-8ed9e212454e.avif';
 
 export default function Home() {
   return (
     <>
       <main>
         <section className="listLink">
-          <Link className="homeLink">
-            <img src="" alt=""></img>
-            <h2>Robe</h2>
-          </Link>
-          <Link className="homeLink">
-            <img src="" alt=""></img>
-            <h2>Top</h2>
-          </Link>
-          <Link className="homeLink">
-            <img src="" alt=""></img>
-            <h2>Pant</h2>
-          </Link>
-          <Link className="homeLink">
-            <img src="" alt=""></img>
-            <h2>Shoes</h2>
-          </Link>
+          <div className="homeLink">
+            <img src={banner} alt=""></img>
+          </div>
+          <div className="homeLink">
+            <Link href="" className="link-loyalty">
+              <img src={image2} alt="Jewelry presentation"></img>
+              <div className="loyalty-text">
+                <h2 className="homeLink-fidelity-title">Programme fidélité</h2>
+                <p className="homeLink-fidelity-paragraph">
+                  Cumulez des points grâce à notre programme de fidélité.
+                </p>
+              </div>
+            </Link>
+          </div>
+          <div className="homeLink">
+            <Link href="">
+              <img src={image3} alt="Jewelry presentation"></img>
+              <h2 className="homeLink homeLink-present">
+                Faites un cadeau avec Girl Shop
+              </h2>
+            </Link>
+          </div>
         </section>
       </main>
     </>
